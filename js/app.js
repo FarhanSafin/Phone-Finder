c49ceb3bf9891a5821aef7f1b6527c49b7210991
FarhanSafin <farhansafin@gmail.com>
2022-03-01 10:11:39 +0600

    Mobile data sliced. Show more button added

--- js/app.js
+++ js/app.js
@@ -9,13 +9,11 @@ const allMobiles = () => {
     fetch(url).then(res => res.json()).then(data => showAllMobiles(data.data));
 }
 
-
-const showAllMobiles = (mobiles) => {
-
-    for(const mobile of mobiles){
-    const container = document.getElementById("mobile-container");
-    const div = document.createElement('div');
-    div.innerHTML = `    <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+const appendData = (data) => {
+    for (const mobile of data) {
+        const container = document.getElementById("mobile-container");
+        const div = document.createElement('div');
+        div.innerHTML = `    <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
         <div>
             <img class="rounded-t-lg mx-auto" src="${mobile.image}" alt="" />
         </div>
@@ -35,13 +33,29 @@ const showAllMobiles = (mobiles) => {
             </div>
         </div>
     </div>`;
-    container.appendChild(div);
+        container.appendChild(div);
     }
 }
 
 
+const showAllMobiles = (mobiles) => {
+    console.log(mobiles.length);
+    let limitedData = mobiles.slice(0, 20);
+    console.log(limitedData);
+    appendData(limitedData);
+    if(mobiles.length > 20){
+        const container = document.getElementById("show-more-button");
+        const showMore = document.createElement('div');
+        showMore.innerHTML = ` <br>           <div
+                class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Show More
+                <svg class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clip-rule="evenodd"></path>
+                </svg>
+            </div>`
+        container.appendChild(showMore);
+    }
 
-
-
-
-
+}
